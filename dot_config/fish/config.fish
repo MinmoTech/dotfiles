0acebaa07f3172e8cae6ccecab7dfed7aedafc1a
Julius Dehner <julius.dehner@gmail.com>
2022-02-09 01:39:54 +0100

    Update dot_config/fish/config.fish Update dot_config/ranger/rc.conf

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -9,10 +9,10 @@ set fish_cursor_default block
 set fish_cursor_insert line
 set fish_cursor_visual block
 
-bind -M insert \e\[1\;6D\x7F  backward-kill-word
-bind -M default \e\[1\;6D\x7F  backward-kill-word
-bind -M insert \e\[1\;2F\x7F  kill-line
-bind -M default \e\[1\;2F\x7F  kill-line
+bind -M insert \e\[1\;6D\x7F backward-kill-word
+bind -M default \e\[1\;6D\x7F backward-kill-word
+bind -M insert \e\[1\;2F\x7F kill-line
+bind -M default \e\[1\;2F\x7F kill-line
 
 
 
@@ -22,9 +22,15 @@ theme.sh iceberg-light
 direnv hook fish | source
 # GoLang
 set GOROOT '/home/julius/.go'
-set GOPATH '/home/julius/go'
+set GOPATH /home/julius/go
 set PYPATH '/home/julius/.local/bin'
 set PATH $GOPATH/bin $GOROOT/bin $PYPATH /home/julius/.cargo/bin $PATH
 source /opt/anaconda/etc/fish/conf.d/conda.fish
 status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
+
+if test -n "$DIRENV_DIR"
+    echo "In DirEnv"
+else if test -f /run/.containerenv
+    echo "Out of DirEnv"
+end
