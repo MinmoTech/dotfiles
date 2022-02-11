b740cf467646c9269a864f26d2f5c4b9f9043d37
Julius Dehner <julius.dehner@gmail.com>
2022-02-11 16:33:50 +0100

    Update Programs/executable_ipfs-add.sh

--- Programs/executable_ipfs-add.sh
+++ Programs/executable_ipfs-add.sh
@@ -6,3 +6,5 @@ FILE=$1
 hash=$(ipfs add -wrQ "$FILE")
 echo "$hash"
 echo "https://dweb.link/ipfs/${hash}/${FILE}" | xclip -sel clip
+echo "$hash" > /tmp/ipfshash
+ssh root@plexiglas.xyz "docker exec ipfs ipfs pin add /ipfs/${hash}" &
