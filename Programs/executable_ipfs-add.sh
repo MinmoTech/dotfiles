f1553571bb751f03547c417ee206a20992c373c3
Julius Dehner <julius.dehner@gmail.com>
2022-02-27 04:28:55 +0100

    Update Programs/executable_ipfs-add.sh

--- Programs/executable_ipfs-add.sh
+++ Programs/executable_ipfs-add.sh
@@ -5,5 +5,5 @@ FILE=$1
 
 hash=$(ipfs add -wrQ "$FILE")
 echo "$hash"
-echo "https://ipfs.io/ipfs/${hash}/${FILE}" | xclip -sel clip
+/bin/echo -n "https://ipfs.io/ipfs/${hash}/${FILE}" | xclip -sel clip
 echo "$hash" > /tmp/ipfshash
