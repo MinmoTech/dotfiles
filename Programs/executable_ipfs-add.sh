e9e99aaa0eb2d016d6f4757fcfd208932ba393c4
Minmo <maroboro.mike@gmail.com>
2022-03-19 18:27:48 +0100

    Update Programs/executable_ipfs-add.sh

--- Programs/executable_ipfs-add.sh
+++ Programs/executable_ipfs-add.sh
@@ -5,5 +5,5 @@ FILE=$1
 
 hash=$(ipfs add --cid-version 1 -wrQ "$FILE")
 echo "$hash"
-/bin/echo -n "https://migaku_test.mypinata.cloud/ipfs/${hash}/${FILE}" | xclip -sel clip
+/bin/echo -n "https://ipfs.io/ipfs/${hash}/${FILE}" | xclip -sel clip
 echo "$hash" > /tmp/ipfshash
