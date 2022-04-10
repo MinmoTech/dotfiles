4ea107ba029a99ed85198b1426c7a2628bbb6554
Minmo <maroboro.mike@gmail.com>
2022-04-10 20:18:43 +0200

    Update dot_config/fish/functions/stream-up.fish

--- dot_config/fish/functions/stream-up.fish
+++ dot_config/fish/functions/stream-up.fish
@@ -1,4 +1,1 @@
-function stream-up --wraps='cd ~/Projects/Project-Lightspeed/ && docker-compose up' --description 'alias stream-up cd ~/Projects/Project-Lightspeed/ && docker-compose up'
-  pactl list short sinks | grep null-sink || pactl load-module module-null-sink;
-  pactl list short sinks | grep combined || pactl load-module module-combine-sink sink_name=combined slaves=(pactl list short sinks | grep null-sink | fex \t1),(pactl list short sinks | grep RUNNING | fex \t2);
-end
+
